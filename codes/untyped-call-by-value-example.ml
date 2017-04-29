bb62b20e065647fa900493ff109be646dcd5be7c
Danny Willems <danny@b2c2.net>
2017-04-29 19:33:39 +0000

    Change example.

--- codes/untyped-call-by-value-example.ml
+++ codes/untyped-call-by-value-example.ml
@@ -1,6 +1,6 @@
-let a = ref 2;;
+let a = ref 0;;
+(* Affiche 0 et 1 *)
+(fun () -> Printf.printf "%d\n" (!a); a := 2)
+  (Printf.printf "%d\n" (!a); a := 1);;
 (* Affiche 2 *)
-(fun () -> Printf.printf "%d\n" (!a); a := 42)
-  (Printf.printf "%d\n" (!a); a := 2);;
-(* Affiche 42 *)
-Printf.printf "%d\n" (!a)
+Printf.printf "%d\n" (!a);;
